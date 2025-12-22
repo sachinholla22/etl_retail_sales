51514d1fbfcbb37e39bee68153d8f771a8106e72
sachin <sachinholla01@gmail.com>
2025-12-22 18:52:57 +0530

    Airflow changes

--- dags/schedule_tasks.py
+++ dags/schedule_tasks.py
@@ -1,1 +1,34 @@
-from 
+from airflow import DAG 
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+from pipelines.extract import upload_to_s3,download_from_s3
+import logging
+from logs.log_config import setup_config
+
+setup_config()
+
+basic_config={
+    
+    "start_date":datetime(2025,12,23),
+    "retries":1
+
+}
+
+
+with DAG(
+   dag_id="testing_airflow_dag",
+   default_args=basic_config,
+   schedule_interval="@daily",
+   catchup=False
+
+) as dag:
+    
+    uploadScript=PythonOperator(
+        task_id="uploadScript",
+        python_callable=upload_to_s3,
+        op_args=[]
+
+    )
+
+
+
--- pipelines/extract.py
+++ pipelines/extract.py
@@ -1,8 +1,9 @@
 import os
 import boto3
 from botocore.exceptions import ClientError
-from log_config import setup_config
+from logs.log_config import setup_config
 import pandas as pd
+import logging
 
 setup_config()
 
@@ -10,9 +11,17 @@ def upload_to_s3(bucket_name,object_name,file_name):
     try:
         logging.info("Started uploading file")
         s3_client=boto3.client('s3')
-        if object_name==None or object_name='':
+        
+        if not object_name:
             object_name=file_name
-        result=s3_client.upload_file(bucket_name,object_name,file_name)
+        try:
+
+            if s3_client.head_object(bucket_name,object_name):
+                logging.info("File already exists in the AWS S3")
+                return 
+        except ClientError as e:
+            pass    
+        result=s3_client.upload_file(file_name,bucket_name,object_name)
         logging.info(f"The file {file_name} uploaded succeessfully")
          
     except ClientError as e:
@@ -24,6 +33,7 @@ def download_from_s3(bucket_name,object_key,local_file_path):
         logging.info("Downloading from s3")
         s3_client=boto3.client('s3')
         if os.path.exists(local_file_path) and os.getsize(local_file_path)>0:
+            logging.info("file already exists in the current location so skipping downloading")
             return 
         res=s3_client.download_file(bucket_name,object_key,local_file_path)
         logging.info(f"File has been donwloaded in the path{local_file_path}")
@@ -32,4 +42,5 @@ def download_from_s3(bucket_name,object_key,local_file_path):
 
 
 def read_csv_file(path):
+    logging.info("Reading a csv file and converting it to dataframe")
     return pd.read_csv(path)
