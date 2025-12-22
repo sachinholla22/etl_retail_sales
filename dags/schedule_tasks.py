ba99a65c3591e75c9afb0dc28be96414d9dc2da8
sachin <sachinholla01@gmail.com>
2025-12-22 22:12:37 +0530

    modified airflow workflow

--- dags/schedule_tasks.py
+++ dags/schedule_tasks.py
@@ -3,9 +3,16 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 from pipelines.extract import upload_to_s3,download_from_s3
 import logging
+import os
 from logs.log_config import setup_config
 
 setup_config()
+local_csv_path=r"C:\Users\sachi\Downloads\hospital_data"
+bucket="amazon-aws-200122"
+object_name="sales/"
+file_name="retail_sales.csv"
+directory=os.getabspath("../datas/raw/")
+local_destination=os.path.join(directory,"local_copy.csv")
 
 basic_config={
     
@@ -26,9 +33,18 @@ with DAG(
     uploadScript=PythonOperator(
         task_id="uploadScript",
         python_callable=upload_to_s3,
-        op_args=[]
+        op_args=[bucket,object_name,file_name],
 
     )
+
+    downloadScript=PythonOperator(
+        task_id="downloadScript",
+        python_callable=download_from_s3,
+        op_args=[bucket,object_name,local_destination],
+
+    )
+
+    uploadScript >> downloadScript
 
 
 
